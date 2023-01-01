fac34b9a4f7a37ebfee2099a30293704c88aaf02
Paul Meffle <meffle.paul@me.com>
2023-01-01 14:28:36 +0100

    Start restructuring work

--- example/main.c
+++ example/main.c
@@ -1,78 +1,11 @@
 #include <flecs-luajit/module.h>
 #include <flecs.h>
 
-static char const* s_init_code =
-        "package.path = package.path .. ';./external/flecs-luajit-binding/?.lua'\n"
-        "require 'flecs' { world = ..., cdef = require 'flecs_cdef' }\n"
-;
-
 int main(void) {
         ecs_world_t* world = ecs_init();
-
         ecs_set_threads(world, 2);
 
-        ECS_IMPORT(world, FlecsConfigLuajit);
-
-        ecs_singleton_set(world, EcsLuajitConfig, {
-                .init_code = (char*) s_init_code,
-                .init_file = "example/init.lua",
-        });
-
-        ECS_IMPORT(world, FlecsLuajit);
-
-        ecs_luajit_ensure_stages(world);
-
-        ecs_entity_t LuajitSystem = ecs_entity_init(world, &(ecs_entity_desc_t) {
-                .name = "LuajitSystem",
-                .add = { ecs_dependson(EcsOnUpdate) },
-        });
-
-        ecs_set(world, LuajitSystem, EcsLuajitSystem, {
-                .query_expr = "Position",
-                //.callback = "LuajitSystem",
-        });
-
-        ecs_set(world, LuajitSystem, EcsLuajitScript, {
-                "print('called system script')\n"
-                "return function(iter)\n"
-                "       for e, p in iter:each() do\n"
-                "               print('[e] ' .. iter:world():name(e))\n"
-                "               print('  {' .. p.x .. ',' .. p.y .. '}')\n"
-                "       end\n"
-                "       print('LuajitSystem()')\n"
-                "end\n"
-                // Alternative:
-                //"function LuajitSystem(iter)\n"
-                //"       print('update()')\n"
-                //"end\n"
-        });
-
-        ecs_entity_t Bob = ecs_entity_init(world, &(ecs_entity_desc_t) {
-                .name = "Bob",
-        });
-
-        ecs_set(world, Bob, EcsLuajitScript, {
-                "print('returning Bob\\'s name')\n"
-                "return 'Bobo'\n"
-        });
-
-        ecs_set_pair(world, Bob, EcsLuajitOnStage, ecs_id(EcsLuajitScript), {
-                .stage_id = 0,
-        });
-
-
-        // Equivalent alternative
-        /*ecs_luajit_system_init(world, &(ecs_luajit_system_desc_t) {
-                .entity = LuajitSystem,
-                .query.filter.expr = "",
-                .callback = "update",
-        });*/
-
-        ecs_progress(world, 0);
-
-        ecs_set(world, Bob, EcsLuajitScript, {
-                "print('Hello again from ' .. Bob .. ' on the next tick')\n"
-        });
+        ECS_IMPORT(world, FlecsLuajitBase);
 
         ecs_progress(world, 0);
 
--- include/flecs-luajit/module.h
+++ include/flecs-luajit/module.h
@@ -6,107 +6,19 @@
 #include <flecs.h>
 #include <lua.h>
 
-FLECS_LUAJIT_API
-extern ECS_COMPONENT_DECLARE(EcsLuajitConfig);
-
-typedef struct EcsLuajitConfig {
-        char* init_code;
-        char* init_file;
-} EcsLuajitConfig;
-
-FLECS_LUAJIT_API
-extern ECS_COMPONENT_DECLARE(EcsLuajitHost);
-
-typedef struct EcsLuajitHost {
-        lua_State** states;
-        int32_t count;
-} EcsLuajitHost;
-
-FLECS_LUAJIT_API
-extern ECS_COMPONENT_DECLARE(EcsLuajitSystem);
-
-typedef struct EcsLuajitSystem {
-        char* query_expr;
-        char* callback;
-        ecs_ftime_t interval;
-        int32_t rate;
-        ecs_entity_t tick_source;
-        bool multi_threaded;
-        bool no_readonly;
-} EcsLuajitSystem;
-
-FLECS_LUAJIT_API
-extern ECS_COMPONENT_DECLARE(EcsLuajitScript);
-
-typedef struct EcsLuajitScript {
-        char* code;
-} EcsLuajitScript;
-
-FLECS_LUAJIT_API
-extern ECS_COMPONENT_DECLARE(EcsLuajitOnStage);
-
-typedef struct EcsLuajitOnStage {
-        int32_t stage_id;
-} EcsLuajitOnStage;
-
-FLECS_LUAJIT_API
-extern ECS_DECLARE(EcsLuajitLoaded);
-
-typedef struct ecs_luajit_system_desc_t {
-        ecs_entity_t entity;
-        ecs_query_desc_t query;
-        char const* callback;
-        void* ctx;
-        ecs_ctx_free_t ctx_free;
-        ecs_ftime_t interval;
-        int32_t rate;
-        ecs_entity_t tick_source;
-        bool multi_threaded;
-        bool no_readonly;
-} ecs_luajit_system_desc_t;
-
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-FLECS_LUAJIT_API
-void ecs_luajit_ensure_stages(
-                ecs_world_t* world);
-
-FLECS_LUAJIT_API
-ecs_entity_t ecs_luajit_system_init(
-                ecs_world_t* world,
-                ecs_luajit_system_desc_t const* desc);
-
-FLECS_LUAJIT_API
-void ecs_luajit_run(
-                ecs_world_t* world,
-                char const* code);
-
-FLECS_LUAJIT_API
-void ecs_luajit_run_on_stage(
-                ecs_world_t* world,
-                int32_t stage_id,
-                char const* code);
-
-FLECS_LUAJIT_API
-int32_t ecs_luajit_iter_term_count(
-                ecs_iter_t const* iter);
-
-FLECS_LUAJIT_API
-char* ecs_luajit_struct_cdef(
-                ecs_world_t const* world,
-                ecs_entity_t type);
 
 // TODO: FlecsLuajitConfig would be prettier but that won't work with the current
 // import function that just checks whether an entity already exists and not
 // whether it has the EcsModule tag.
 FLECS_LUAJIT_API
-void FlecsConfigLuajitImport(
+void FlecsLuajitConfigImport(
                 ecs_world_t* world);
 
 FLECS_LUAJIT_API
-void FlecsLuajitImport(
+void FlecsLuajitBaseImport(
                 ecs_world_t* world);
 
 #ifdef __cplusplus
